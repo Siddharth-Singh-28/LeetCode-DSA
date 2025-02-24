53d78a39bb7c6ff3be37e622b958fe2d70618c78
Siddharth Singh <114818090+Siddharth-Singh-28@users.noreply.github.com>
2025-02-24 09:02:23 +0530

    Time: 0 ms (100%), Space: 42.3 MB (49.87%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,36 +1,26 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-      int m = matrix.length;
-      // base case condition
-      if(m==0){
-        return false;
-      }  
-      // Number of columns
-      int n = matrix[0].length;
-
-      // Binary search implementation
-      int low=0, high = m*n-1;
-      int Mid_idx, Mid_Element, row_idx, col_idx;
-      while(low<=high){
-        Mid_idx = low+(high-low)/2;
-        row_idx = Mid_idx/n;
-        col_idx = Mid_idx%n;
-
-        Mid_Element = matrix[row_idx][col_idx];
-        if(target == Mid_Element){
-            return true;
-        }
-        else{
-            if(Mid_Element <target ){
-                low = Mid_idx+1;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int start = 0;
+        int end = m * n - 1;
+        while(start <= end){
+            int mid_index = start + (end - start)/2;
+            int row_index = mid_index / n;
+            int col_index = mid_index % n;
+            int mid_element = matrix[row_index][col_index];
+            if(target == mid_element){
+                return true;
             }
             else{
-                high = Mid_idx-1;
-
+                if(mid_element < target){
+                    start = mid_index + 1;
+                }
+                else{
+                    end = mid_index - 1;
+                }
             }
-
         }
-      }
-      return false;
+        return false;
     }
 }
