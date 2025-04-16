3db32a3222b0b71615cc28bdd54477ed214f3816
Siddharth Singh <114818090+Siddharth-Singh-28@users.noreply.github.com>
2025-04-16 18:32:50 +0530

    Time: 1 ms (99.69%), Space: 45.6 MB (31.61%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.java
@@ -1,12 +1,12 @@
 class Solution {
     public String largestOddNumber(String num) {
         int i = num.length() - 1;
-        while(num.length()>0){
-            if(num.charAt(i)=='1'||num.charAt(i)=='3'||num.charAt(i)=='5'||num.charAt(i)=='7'||num.charAt(i)=='9'){
-                return num;
+        while (i >= 0) {
+            char ch = num.charAt(i);
+            if ((ch - '0') % 2 == 1) {
+                return num.substring(0, i + 1);
             }
             i--;
-            num = num.substring(0, num.length()-1);
         }
         return "";
     }
