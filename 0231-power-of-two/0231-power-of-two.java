74f08a46366898ecbecbfb06f136f172eb0e65ee
Siddharth Singh <114818090+Siddharth-Singh-28@users.noreply.github.com>
2025-07-04 21:56:07 +0530

    Time: 1 ms (36.02%), Space: 40.8 MB (79.54%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,13 +1,12 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        return recursiveFunction(n, 0);
-    }
-    public boolean recursiveFunction(int n, int i){
-        if(n<Math.pow(2,i))
-            return false;
-        else if(n==Math.pow(2,i))
-            return true;
-        else
-            return recursiveFunction(n, i+1);
+        int i = 0;
+        while(n>=Math.pow(2,i)){
+            if(Math.pow(2, i)==n){
+                return true;
+            }
+            i++;
+        }
+        return false;
     }
 }
