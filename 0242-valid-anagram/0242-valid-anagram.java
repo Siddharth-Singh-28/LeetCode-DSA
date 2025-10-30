3137b1803529b75c7959771c19d9f4a7f7ab7628
Siddharth Singh <114818090+Siddharth-Singh-28@users.noreply.github.com>
2025-10-30 18:59:11 +0530

    Time: 15 ms (23.96%), Space: 44.6 MB (49.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -5,7 +5,7 @@ class Solution {
         }
         HashMap<Character, Integer> map = new HashMap<>();
         for(char c : s.toCharArray()){
-            map.put(c, map.getOrDefault(c,0) + 1);
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
         for(char c : t.toCharArray()){
             if(!map.containsKey(c) || map.get(c) == 0){
