4c23910298b9f7a975b685e61c3104bc007b5a47
Siddharth Singh <114818090+Siddharth-Singh-28@users.noreply.github.com>
2025-10-30 19:06:50 +0530

    Time: 4 ms (55.39%), Space: 45.1 MB (49.54%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -5,10 +5,10 @@ class Solution {
             map.put(nums[i], i);
         }
         for(int i=0; i<nums.length; i++){
-            if(map.containsKey( target - nums[i]) && map.get( target - nums[i]) != i){
-                return new int[]{i, map.get(target - nums[i])};
-                }  
+            if(map.containsKey(target - nums[i]) && map.get(target - nums[i]) != i){
+                return new int[] {i, map.get(target-nums[i])};
             }
+        }
         return new int[]{};
     }
 }
