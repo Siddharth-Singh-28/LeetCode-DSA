5b1bcdf86ec41b2d3bec862c4d9ef787069fadc8
Siddharth Singh <114818090+Siddharth-Singh-28@users.noreply.github.com>
2026-02-18 20:53:34 +0530

    Time: 34 ms (59.67%), Space: 59.5 MB (33.46%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,32 +1,35 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-        for(int i=0; i<nums.length; i++){
-            if(i > 0 && nums[i] == nums[i-1]){
+        List<List<Integer>> outerList = new ArrayList<>();
+        for(int i=0; i<nums.length-2; i++) {
+            if(i>0 && nums[i] == nums[i-1]) {
                 continue;
             }
-
-            int j = i + 1;
-            int k = nums.length - 1;
-
-            while(j<k){
-                int total = nums[i] + nums[j] + nums[k];
-                if(total==0){
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-                    j++;
-                    while(nums[j] == nums[j-1] && j < k){
-                        j++;
+            int left = i+1;
+            int right = nums.length - 1;
+            int sum = -1 * nums[i];
+            while(left < right) {
+                int s = nums[left] + nums[right];
+                if(s == sum) {
+                    outerList.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+                    while(left < nums.length && left == nums[left-1]) {
+                        left++;
+                    }
+                    while(right >= 0 && nums[right] == nums[right+1]) {
+                        right--;
                     }
                 }
-                else if(total<0){
-                    j++;
+                else if(s < sum) {
+                    left++;
                 }
-                else{
-                    k--;
+                else {
+                    right--;
                 }
-            }
+            } 
         }
-        return res;
+        return outerList;
     }
 }
