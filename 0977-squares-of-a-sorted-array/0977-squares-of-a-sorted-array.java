10f612b29f8228cfe0e8a1cf9a6cbbe04a8e487c
Siddharth Singh <114818090+Siddharth-Singh-28@users.noreply.github.com>
2026-02-18 15:00:31 +0530

    Time: 1 ms (99.81%), Space: 47.1 MB (94.42%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,36 +1,20 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        int[] result = new int[nums.length];
-        int index = 0;
-        int pointer1 = 0;
-        int pointer2 = 0;
-        while(pointer1 < nums.length) {
-            if(nums[pointer1] < 0) {
-                pointer1++;
+        int start = 0;
+        int end = nums.length - 1;
+        int result[] = new int[nums.length];
+
+        for(int i = nums.length - 1; i >= 0; i--) {
+            if(Math.abs(nums[start]) > Math.abs(nums[end])) {
+                result[i] = nums[start] * nums[start];
+                start++;
             }
             else {
-                break;
+                result[i] = nums[end] * nums[end];
+                end--;
             }
         }
-        pointer2 = pointer1 - 1;
-        while(pointer2 >= 0 && pointer1 < nums.length) {
-            if(Math.pow(nums[pointer2], 2) > Math.pow(nums[pointer1], 2)) {
-                result[index++] = (int)Math.pow(nums[pointer1], 2);
-                pointer1++;
-            }
-            else {
-                result[index++] = (int)Math.pow(nums[pointer2], 2);
-                pointer2--;
-            }
-        }
-        while(pointer2 >= 0) {
-            result[index++] = (int)Math.pow(nums[pointer2], 2);
-            pointer2--;
-        }
-        while(pointer1 < nums.length) {
-            result[index++] = (int)Math.pow(nums[pointer1], 2);
-            pointer1++;
-        }
+
         return result;
     }
 }
